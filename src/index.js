73190e1d51d80a1d10b904f8908bf2c52c70dba9
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-05-07 20:52:36 -0400

    adding main scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './JournalApp'
 // cargar sass
-import './styles/styles.scss'
+import './styles/main.scss'
 
 ReactDOM.render(
     <App />,

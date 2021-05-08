bd3bd62a6c5932858c4f49c66e06bb30a784f1db
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-05-07 21:36:17 -0400

    adding btn google auth

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,20 +1,31 @@
 const LoginScreen = () => {
-    return ( 
-        <>
-        <h2>Login Screen</h2>
+  return (
+    <>
+      <h2>Login Screen</h2>
 
-          <form>
-              <input type="text" placeholder='email' name='email' />
-              <input type="password" placeholder='password' name='password' />
+      <form>
+        <input type="text" placeholder="email" name="email" />
+        <input type="password" placeholder="password" name="password" />
 
-              <input type="submit" value="login" />
+        <input type="submit" value="login" />
 
-              <hr />
+        <hr />
 
-              google
-          </form>
-        </>
-     );
-}
- 
+        <div className="google-btn">
+          <div className="google-icon-wrapper">
+            <img
+              className="google-icon"
+              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              alt="google button"
+            />
+          </div>
+          <p className="btn-text">
+            <b>Sign in with google</b>
+          </p>
+        </div>
+      </form>
+    </>
+  );
+};
+
 export default LoginScreen;

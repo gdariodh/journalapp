ed0ae1b2c2d226b050f2278a9572dfa76769734b
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-05-07 20:41:56 -0400

    adding routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,19 @@
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-import AuthRouter from "./AuthRouter";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 //   children route
+import AuthRouter from "./AuthRouter";
 // components
+import JournalScreen from "../components/journal/JournalScreen";
 
 const AppRouter = () => {
-    return ( 
-        <Router>
-            <Switch>
-                <Route to="/auth" component={AuthRouter}/>
-            </Switch>
-        </Router>
-     );
-}
- 
+  return (
+    <Router>
+      <Switch>
+        <Route path="/auth" component={AuthRouter} />
+        <Route exact path="/" component={JournalScreen} />
+        <Redirect to="/auth/login" />
+      </Switch>
+    </Router>
+  );
+};
+
 export default AppRouter;
--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -1,13 +1,17 @@
-import {
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
+import LoginScreen from "../components/auth/LoginScreen";
+import RegisterScreen from "../components/auth/RegisterScreen";
 
 const AuthRouter = () => {
-    return ( 
-        <p>holi</p>
-     );
-}
- 
+  return (
+    <>
+      <Switch>
+        <Route exact path="/auth/login" component={LoginScreen} />
+        <Route exact path="/auth/register" component={RegisterScreen} />
+        <Redirect to="/auth/login" />
+      </Switch>
+    </>
+  );
+};
+
 export default AuthRouter;

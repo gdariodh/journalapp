f8aa77c6576aa1f1cf9dffd937d5bba8538ecd81
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-05-10 21:22:16 -0400

    finishing NoteScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,12 +1,30 @@
 import { NotesAppBar } from "./NotesAppBar";
 
-
 const NoteScreen = () => {
-    return ( 
-        <div className="notes__main-content">
-          <NotesAppBar/>
-        </div>
-     );
-}
- 
+  return (
+    <div className="notes__main-content">
+      <NotesAppBar />
+
+        <form className="notes__content">
+          <input
+            autoComplete="off"
+            className="notes__title-input"
+            type="text"
+            placeholder="Some awesome title"
+          />
+          <textarea
+            placeholder="What happened today?"
+            className="notes__text-area"
+          ></textarea>
+          <div className="notes__image">
+            <img
+              src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+              alt="una imagen"
+            />
+          </div>
+        </form>
+    </div>
+  );
+};
+
 export default NoteScreen;

4ea02d247409bf01e92f81dc7c0cbf0ed4354c4a
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-05-13 01:08:04 -0400

    adding store

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,11 +1,16 @@
 import AppRouter from "./routers/AppRouter";
-
-
+import { Provider } from "react-redux";
+// encapsula todos los reducers en un store, para hacerlos disponibles en toda la app
+import {store} from "./redux/store";
 
 const JournalApp = () => {
-    return ( 
-        <AppRouter/>
-     );
-}
- 
+  return (
+    <>
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    </>
+  );
+};
+
 export default JournalApp;

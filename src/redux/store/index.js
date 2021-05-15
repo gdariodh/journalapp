76c742f7b4eaff1217ee94bc09778834fb151e13
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-05-14 21:42:39 -0400

    adding thunk as applyMiddleware in store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,11 +1,20 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import { authReducer } from "../reducers/authReducer";
 
+// para poder colocar redux como applyMiddleware en store
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+// combina todos los reducer en uno
 const reducers = combineReducers({
   auth: authReducer,
 });
 
+// levantamos redux en la app
 export const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );

84da62109a28eca245f6ced4e005774cc80937e1
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-05-21 23:53:33 -0400

    adding uiReducer to store redux

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { authReducer } from "../reducers/authReducer";
+import { uiReducer } from "../reducers/uiReducer";
 
 // para poder colocar redux como applyMiddleware en store
 const composeEnhancers =
@@ -11,6 +12,7 @@ const composeEnhancers =
 // combina todos los reducer en uno
 const reducers = combineReducers({
   auth: authReducer,
+  ui: uiReducer
 });
 
 // levantamos redux en la app

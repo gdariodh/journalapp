68ad405c3ef28a6c50d1af2c36295305bcd9bcd1
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-05-21 21:50:38 -0400

    adding read form

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,40 +1,82 @@
+import { useState } from "react";
+
 const { Link } = require("react-router-dom");
 
 const RegisterScreen = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    repassword: "",
+  });
+
+  const { name, email, password, repassword } = data;
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== repassword)
+      return alert("Los password son diferentes, intente de nuevo");
+
+    console.log(data);
+
+    setData({
+      name: "",
+      email: "",
+      password: "",
+      repassword: "",
+    });
+  };
+
   return (
     <>
       <h2 className="auth__title">Register Screen</h2>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
+          onChange={handleChange}
           autoComplete="off"
           className="auth__input"
           type="text"
           placeholder="name"
           name="name"
+          value={name}
         />
 
         <input
+          onChange={handleChange}
           autoComplete="off"
           className="auth__input"
           type="text"
           placeholder="email"
           name="email"
+          value={email}
         />
         <input
+          onChange={handleChange}
           autoComplete="off"
           className="auth__input"
           type="password"
           placeholder="password"
           name="password"
+          value={password}
         />
 
         <input
+          onChange={handleChange}
           autoComplete="off"
           className="auth__input"
           type="password"
           placeholder="confirm password"
-          name="password"
+          name="repassword"
+          value={repassword}
         />
 
         <input

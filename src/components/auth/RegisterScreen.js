d941a125d1c1e1ccff8415a681f4a1d2234bba9c
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-05-22 00:15:18 -0400

    adding dispatch for uiAction

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 // npm validator
-import validator from 'validator'
+import validator from "validator";
+// redux
+import { useDispatch } from "react-redux";
+import { setError } from "../../redux/actions/uiAction";
 
 const { Link } = require("react-router-dom");
 
 const RegisterScreen = () => {
+  const dispatch = useDispatch();
+
   const [data, setData] = useState({
     name: "",
     email: "",
@@ -23,25 +28,37 @@ const RegisterScreen = () => {
 
   // validaciones manuales con libreria de npm validator
   const isFormValid = () => {
-     if(name.trim().length === 0){
-       console.log('Name is required');
-       return false;
-     }else if(!validator.isEmail(email)){
-       console.log('Email isnt valid');
-       return false;
-     }else if(password !== repassword || password.length < 5 || repassword.length < 5){
-       console.log('Password should be at least 6 characters and match each other');
-       return false;
-     }
+    if (name.trim().length === 0) {
+      //  console.log('Name is required');
+      dispatch(setError("Name is required"));
+      return false;
+    } else if (!validator.isEmail(email)) {
+      //  console.log('Email isnt valid');
+      dispatch(setError("Email isnt valid"));
+
+      return false;
+    } else if (
+      password !== repassword ||
+      password.length < 5 ||
+      repassword.length < 5
+    ) {
+      //  console.log('Password should be at least 6 characters and match each other');
+      dispatch(
+        setError(
+          "Password should be at least 6 characters and match each other"
+        )
+      );
+      return false;
+    }
 
     return true;
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(isFormValid()){
-      console.log('Form correcto');
+    if (isFormValid()) {
+      console.log("Form correcto");
     }
 
     setData({
@@ -57,11 +74,7 @@ const RegisterScreen = () => {
       <h2 className="auth__title">Register Screen</h2>
 
       <form onSubmit={handleSubmit}>
-
-        <div className="auth__alert-error">
-          hola
-        </div>
-
+        <div className="auth__alert-error">hola</div>
 
         <input
           onChange={handleChange}

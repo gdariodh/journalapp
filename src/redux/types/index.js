9ebad6b7248454a4761fa336db759e2b3b0bed71
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-05-21 23:53:05 -0400

    adding typesUi

--- src/redux/types/index.js
+++ src/redux/types/index.js
@@ -1,4 +1,9 @@
 export const types = {
     login: '[Auth] Login',
-    logout: '[Auth] Logout'
+    logout: '[Auth] Logout',
+}
+
+export const typesUi = {
+    uiSetError: '[UI] Set Error',
+    uiRemoveError: '[UI] Remove Error'
 }

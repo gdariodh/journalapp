98fc9695bc2a3fda9392f6269d45c9dc190f171f
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-06-03 00:49:14 -0400

    adding onAuthStateChanged

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,33 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 //   children route
 import AuthRouter from "./AuthRouter";
 // components
 import JournalScreen from "../components/journal/JournalScreen";
+import { useEffect } from "react";
+import { firebase } from "../firebase/firebaseConfig";
+// redux
+import { useDispatch } from "react-redux";
+import { login } from "../redux/actions/authAction";
 
 const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // revisa si existe user, y lo loguea apenas cargue la app
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        // mandamos el user encontrado y lo logueamos
+        // login(uid, displayName)
+        dispatch(login(user.uid, user.displayName));
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <Switch>

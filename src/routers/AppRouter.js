32fa4ac1727f18522be67f6670da28517d3319b2
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-06-04 20:44:42 -0400

    adding loading component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -8,7 +8,7 @@ import {
 import AuthRouter from "./AuthRouter";
 // components
 import JournalScreen from "../components/journal/JournalScreen";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { firebase } from "../firebase/firebaseConfig";
 // redux
 import { useDispatch } from "react-redux";
@@ -17,6 +17,9 @@ import { login } from "../redux/actions/authAction";
 const AppRouter = () => {
   const dispatch = useDispatch();
 
+  // hacer loading global de la app, pone en espera al user mientras comprueba si tiene un tokenAuth
+  const [checking, setChecking] = useState(true);
+
   useEffect(() => {
     // revisa si existe user, y lo loguea apenas cargue la app
     firebase.auth().onAuthStateChanged((user) => {
@@ -24,10 +27,19 @@ const AppRouter = () => {
         // mandamos el user encontrado y lo logueamos
         // login(uid, displayName)
         dispatch(login(user.uid, user.displayName));
+
+        setChecking(false);
       }
     });
     // eslint-disable-next-line
-  }, []);
+  }, [checking]);
+
+  // retorna una pantalla de carga - un component
+  if (checking) {
+    return <h1>Espere...</h1>;
+
+    //  return (<LoadComponent/>)
+  }
 
   return (
     <Router>

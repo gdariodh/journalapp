2bbb6bff323977d5d18e6afabb87e5a110261b16
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-07-13 21:11:11 -0400

    adding conditionals components with Notes

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -2,17 +2,18 @@ import Sidebar from "./Sidebar";
 // cuando no haya nada seleccionado
 import NothingSelected from "./NothingSelected";
 import NoteScreen from "../notes/NoteScreen";
+// redux
+import { useSelector } from "react-redux";
 
 const JournalScreen = () => {
+  // extraemos el state active de nuestro state principal de notes
+  const { active } = useSelector((state) => state.notes);
+
   return (
     <div className="journal__main-content">
-      
-      <Sidebar/>
+      <Sidebar />
 
-      <main>
-        <NoteScreen/>
-          {/* <NothingSelected/> */}
-      </main>
+      <main>{active ? <NoteScreen /> : <NothingSelected />}</main>
     </div>
   );
 };

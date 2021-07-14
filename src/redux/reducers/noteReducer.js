3e625a586a4ea6cd2047f9b75390c54c95388d70
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-07-13 20:57:47 -0400

    initializing noteReducer

---
+++ src/redux/reducers/noteReducer.js
@@ -0,0 +1,14 @@
+import { types } from "../types";
+
+const initialState = {
+    notes: [],
+    active: null,
+}
+
+export const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};

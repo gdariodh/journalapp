f90fb59bfc18035bb3ed446d7617c6d97c2cd6a7
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-07-13 21:26:46 -0400

    added typesNotes

--- src/redux/types/index.js
+++ src/redux/types/index.js
@@ -16,3 +16,13 @@ export const typesUi = {
 }
 
 // NOTES
+export const typesNotes = {
+  notesAddNew: "[Notes] New note",
+  notesActive: "[Notes] Set active note",
+  notesLoad: "[Notes] Load notes",
+  notesUpdated: "[Notes] Updated note",
+  notesFileUrl: "[Notes] Updated image url",
+  notesDelete: "[Notes] Delete note",
+  // cuando el usuario salga de la app y limpiamos la info en memoria
+  notesLogoutCleaning: "[Notes] Logout cleaning",
+};
